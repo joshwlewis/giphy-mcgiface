7cc472d659da2bf75d6135b4ed7ef08d4fce4865
Josh W Lewis <josh.w.lewis@gmail.com>
2016-10-28 10:31:49 -0500

    Don't try to create a database for every test

--- mix.exs
+++ mix.exs
@@ -49,6 +49,6 @@ defmodule GiphyMcgiface.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.migrate", "test"]]
   end
 end
